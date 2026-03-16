b5abff69c6eacf16253612816d6dcb9c0df9f302
alexkay7133 <rsxr0509@leeds.ac.uk>
2026-03-16 12:52:55 +0000

    Finished session 1

--- session-1/task1/student_record.c
+++ session-1/task1/student_record.c
@@ -16,11 +16,13 @@
  * param student The student whose average mark is to be calculated
  * return float The average mark
  */
-float calculate_average(struct student_record student) {
-    // TODO: Implement this function to calculate and return the average
-    // of the three module marks
+float calculate_average(Student student) {
+    float sum = 0;
+    for (int i = 0; i < NUM_MODULES; i++) {
+        sum += student.marks[i] / NUM_MODULES;
+    }
     
-    return 0.0; // Placeholder return value
+    return sum;
 }
 
 /**
@@ -28,21 +30,28 @@ float calculate_average(struct student_record student) {
  * 
  * student The student whose information is to be displayed
  */
-void display_student(struct student_record student) {
+void display_student(Student student) {
     // TODO: Implement this function to display student information
     // Format: Name, ID, individual marks, and average mark
+    printf("Name = %s\n", student.name);
+    printf("ID = %d\n", student.id);
+    printf("Individual marks = %.1f, %.1f, %.1f\n", student.marks[0], student.marks[1], student.marks[2]);
+    printf("Average mark = %.2f\n", calculate_average(student));
 }
 
 /*
  * main
  */
 int main( void ) {
-    // TODO: Create and initialise a student variable with sample data
-    // Example: name = "John Smith", id = 12345, marks = {75.0, 68.5, 81.0}
-    
-    struct student_record student;
+    Student student = {
+        .name = "John Smith",
+        .id =  12345,
+        .marks =  {75.0, 68.5, 81.0}
+    };
+
     
     // TODO: display the student data and average mark
+    display_student(student);
     
     return 0;
 }
--- session-1/task3/shapes.c
+++ session-1/task3/shapes.c
@@ -6,28 +6,35 @@ int main( void ) {
 
     // complete the structure definition in the header file first
     // code in main to test the structures and functions
-    
+    Point p = {.x = 12, .y = 5};
+    Rectangle rec = makeRectangle(p, 7, 4);
+    float rec_area = area(rec);
+    printf("%.1f\n", rec_area);
     return 0;
 }
 
 Rectangle makeRectangle( Point p, float width, float height ){
-    Rectangle r;
-
+    Rectangle r = {
+        .p = p,
+        .width = width,
+        .height = height
+    };
     return r;
 }
 
 float area( Rectangle r ) {
-    float a = 0.0;
-
+    float a = r.width * r.height;
     return a;
 }
 
 void shiftRectangle( Rectangle *r, Point dp ) {
-
+    r->p.x+=dp.x;
+    r->p.y+=dp.y;
     return;
 }
 
 void scaleRectangle( Rectangle *r, float scale ) {
-
+    r->height *= scale;
+    r->width *= scale;
     return;
 }
